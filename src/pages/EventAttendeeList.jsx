83a7f3c866b32e6d1e8f89254018df95aff5f1b2
adamgofast <adam@gofastcrushgoals.com>
2025-10-13 10:10:54 -0400

    Fix EventAttendeeList: add missing EditableFieldComponent import

--- src/pages/EventAttendeeList.jsx
+++ src/pages/EventAttendeeList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../lib/api";
+import EditableFieldComponent from "../components/EditableFieldComponent.jsx";
 
 export default function EventAttendeeList() {
   const { eventId } = useParams();
