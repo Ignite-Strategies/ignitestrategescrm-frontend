c9d5be0cb041b35f357737569ea87073e64cb618
adamgofast <adam@gofastcrushgoals.com>
2025-10-14 12:03:01 -0400

    FIX: Dashboard imports signOutUser from firebase not googleAuth

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../lib/api";
 import { getOrgId } from "../lib/org";
-import { signOutUser } from "../lib/googleAuth";
+import { signOutUser } from "../firebase";
 
 export default function Dashboard() {
   const orgId = getOrgId();
