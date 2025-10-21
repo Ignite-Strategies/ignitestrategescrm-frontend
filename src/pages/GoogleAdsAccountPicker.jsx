2905351b5863fdc55c041b1777c6275d769a7973
adamgofast <adam@gofastcrushgoals.com>
2025-10-21 12:37:47 -0400

    Fix missing closing tags in GoogleAdsAccountPicker

--- src/pages/GoogleAdsAccountPicker.jsx
+++ src/pages/GoogleAdsAccountPicker.jsx
@@ -232,7 +232,8 @@ export default function GoogleAdsAccountPicker() {
               </div>
             </button>
           ))}
-        </div>
+          </div>
+        )}
 
         {/* Info Box */}
         <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
