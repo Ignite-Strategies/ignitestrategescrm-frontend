0ecd3bae34fb050b04394dbe5575e410b07e1b90
adamgofast <adam@gofastcrushgoals.com>
2025-10-21 06:12:49 -0400

    WIP: Fix more import issues - core OAuth system ready for testing

--- src/pages/Outreach.jsx
+++ src/pages/Outreach.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../lib/api";
-import { signInWithGoogle } from "../lib/googleAuth";
+import { connectGmail } from "../lib/googleAuth";
 
 export default function Outreach() {
   const navigate = useNavigate();
